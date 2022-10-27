7139c86a0006f28255b05daacb8086ace3cf991b
Blessing U <blessing986@users.noreply.github.com>
2022-10-27 22:08:33 +0100

    Chore: Styled and added a profile section to the webpage in mobile view

--- src/App.js
+++ src/App.js
@@ -7,6 +7,28 @@ function App() {
 
     <div>
 
+      <img src="./assets/menu.png" className="menu" />
+
+      <img src="./assets/_Avatar share button.png" className="share" />
+
+      <section className="wrapper">
+
+        {/* profile section */}
+
+        <div className="profile">
+
+          <img src="./assets/profile__img.png" alt="" id="profile__img" />
+
+          <p id="twitter">_bleccze_</p>
+
+          <p id="slack">Blessing_U</p>
+
+        </div>
+
+        {/* profile section ends */}
+
+      </section>
+
     </div>
 
   );
