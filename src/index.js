6aeb1f5083337eca339c92b3109905a5a4202a3f
Blessing U <blessing986@users.noreply.github.com>
2022-11-04 20:52:31 +0000

    chore: Added path to the contact page in the index file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './index.css';
 import App from './App';
+import Contact from './pages/contact';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+        
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
